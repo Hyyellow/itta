c0b99a1fae45506f7384813b7175e99bc4b13936
YaYaHuang <570911275@qq.com>
2020-04-04 10:20:13 +0800

    添加Mapper扫描

--- src/main/java/com/program/itta/IttaApplication.java
+++ src/main/java/com/program/itta/IttaApplication.java
@@ -1,9 +1,11 @@
 package com.program.itta;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.program.itta.mapper")
 public class IttaApplication {
 
     public static void main(String[] args) {
