a8a3fa4d122c8317a70d903c29c2f624302494f2
YaYaHuang <570911275@qq.com>
2020-04-04 09:57:49 +0800

    Druid连接池配置

---
+++ src/test/java/com/program/itta/HorseApplicationTests.java
@@ -0,0 +1,44 @@
+package com.program.itta;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @program: itta
+ * @description: Druid配置文件是否成功测试
+ * @author: Mr.Huang
+ * @create: 2020-04-04 09:55
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HorseApplicationTests {
+    /**
+     * Spring Boot 默认已经配置好了数据源，程序员可以直接 DI 注入然后使用即可
+     */
+    @Resource
+    DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws SQLException {
+
+        System.out.println("数据源>>>>>>" + dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+
+        System.out.println("连接>>>>>>>>>" + connection);
+        System.out.println("连接地址>>>>>" + connection.getMetaData().getURL());
+
+        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+        System.out.println("druidDataSource 数据源最大连接数：" + druidDataSource.getMaxActive());
+        System.out.println("druidDataSource 数据源初始化连接数：" + druidDataSource.getInitialSize());
+
+        connection.close();
+    }
+}
