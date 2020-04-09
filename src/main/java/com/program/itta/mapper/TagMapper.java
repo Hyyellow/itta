11d7caaad8e9fffc52f01d7359651c2925fdd497
Boyle <2018770887@qq.com>
2020-04-09 15:54:04 +0800

    有关标签的建表

---
+++ src/main/java/com/program/itta/mapper/TagMapper.java
@@ -0,0 +1,17 @@
+package com.program.itta.mapper;
+
+import com.program.itta.domain.entity.Tag;
+
+import java.util.List;
+
+public interface TagMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Tag record);
+
+    Tag selectByPrimaryKey(Integer id);
+
+    List<Tag> selectAll();
+
+    int updateByPrimaryKey(Tag record);
+}
