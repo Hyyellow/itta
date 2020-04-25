e0fa5402b379eb5f116660bdec0474d885f0c7d8
YaYaHuang <570911275@qq.com>
2020-04-25 16:41:04 +0800

    解决Redis与ES冲突问题

--- src/main/java/com/program/itta/IttaApplication.java
+++ src/main/java/com/program/itta/IttaApplication.java
@@ -1,14 +1,18 @@
 package com.program.itta;
+import	java.awt.Container;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 @MapperScan("com.program.itta.mapper")
 public class IttaApplication {
 
     public static void main(String[] args) {
+        System.setProperty("es.set.netty.runtime.available.processors","false");
         SpringApplication.run(IttaApplication.class, args);
     }
 
--- src/main/java/com/program/itta/domain/entity/Schedule.java
+++ src/main/java/com/program/itta/domain/entity/Schedule.java
@@ -1,13 +1,7 @@
 package com.program.itta.domain.entity;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.experimental.Tolerate;
-
 import java.util.Date;
 
-@Data
-@Builder
 public class Schedule {
     private Integer id;
 
@@ -27,7 +21,75 @@ public class Schedule {
 
     private Date updateTime;
 
-    @Tolerate
-    public Schedule() {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place == null ? null : place.trim();
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime(Date completionTime) {
+        this.completionTime = completionTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
     }
 }
