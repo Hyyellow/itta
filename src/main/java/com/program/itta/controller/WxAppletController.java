d3d7c144910a8409c674cf583c91a819860f562a
YaYaHuang <570911275@qq.com>
2020-05-14 21:59:11 +0800

    登录方法参数接口文档

--- src/main/java/com/program/itta/controller/WxAppletController.java
+++ src/main/java/com/program/itta/controller/WxAppletController.java
@@ -5,6 +5,7 @@ import com.program.itta.common.jwt.JwtFilter;
 import com.program.itta.service.WxAppletService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,13 +31,15 @@ public class WxAppletController {
 
     @Resource
     private JwtConfig jwtConfig;
+
     /**
      * 微信小程序端用户登陆api
      * 返回给小程序端 自定义登陆态 token
      */
     @ApiOperation(value = "用户登录", notes = "(用于用户登录时，获取token)")
     @PostMapping("/api/wx/user/login")
-    public ResponseEntity wxAppletLoginApi(@RequestBody Map<String, String> request) {
+    public ResponseEntity wxAppletLoginApi(@ApiParam(name = "用户请求", value = "用户code", required = true)
+                                           @RequestBody Map<String, String> request) {
         if (!request.containsKey("code") || request.get("code") == null || "".equals(request.get("code"))) {
             Map<String, String> result = new HashMap<>();
             result.put("msg", "缺少参数code或code不合法");
